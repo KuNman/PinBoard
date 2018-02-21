b10fa2c66bcfe0e7aab34b2085220ba403e6e046
KuNman <knejman56@gmail.com>
2018-02-21 19:03:43 +0100

    Extracted basic twig template

--- PinBoard/assets/js/app.js
+++ PinBoard/assets/js/app.js
@@ -4,16 +4,6 @@ var $ = require('jquery');
 
 $(document).ready(function() {
 
-    $('#start:first-child').on('select2:select', function (e) {
-        var type = $('.select2-selection__rendered').text();
-        $('#type option').val(type);
-    });
-
-    $('#start').select2({
-        dropdownAutoWidth : true,
-        placeholder: 'Please select kind',
-        width: '100%'
-    });
 
 
  });
